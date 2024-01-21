3bfb266f84bc84d69efbfcfb56777a2d99269c8a
Jip J. Dekker <jip@dekker.one>
2024-01-21 23:29:58 +1100

    Add the start of a command line application that parses FlatZinc JSON

--- crates/fzn-huub/src/main.rs
+++ crates/fzn-huub/src/main.rs
@@ -1,3 +1,82 @@
-fn main() {
-	println!("Hello, world!");
+use std::{fs::File, io::BufReader, path::PathBuf, time::Duration};
+
+use clap::Parser;
+use flatzinc_serde::FlatZinc;
+use miette::{IntoDiagnostic, Result, WrapErr};
+use tracing::Level;
+use tracing_subscriber::fmt::{time::uptime, SubscriberBuilder};
+
+/// fzn-huub entry point
+///
+/// This function performs command-line parsing and starts the solving process
+/// based on the arguments found.
+fn main() -> Result<()> {
+	// Parse commandline arguments
+	let args = Args::parse();
+
+	// Enable tracing functionality
+	let with_level = |builder: SubscriberBuilder| -> SubscriberBuilder {
+		if args.verbose != 0 {
+			builder.with_max_level(match args.verbose {
+				1 => Level::INFO,
+				_ => Level::DEBUG,
+			})
+		} else {
+			builder
+		}
+	};
+	with_level(tracing_subscriber::fmt())
+		.with_timer(uptime())
+		.finish();
+
+	// Parse FlatZinc JSON file
+	let rdr = BufReader::new(
+		File::open(&args.path)
+			.into_diagnostic()
+			.wrap_err(format!("Unable to open file “{}”", args.path.display()))?,
+	);
+	let fzn: FlatZinc = serde_json::from_reader(rdr)
+		.into_diagnostic()
+		.wrap_err(format!(
+			"Unable to parse file “{}” as FlatZinc JSON",
+			args.path.display()
+		))?;
+
+	println!("{:?}", fzn);
+	Ok(())
+}
+
+/// FlatZinc command line interface for the Huub solver
+///
+/// This interface is intended to connect Huub with MiniZinc
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+	/// Path to the FlatZinc JSON input file
+	#[arg(required = true)]
+	path: PathBuf,
+
+	/// Output intermediate solutions
+	#[arg(short, long)]
+	intermediate_solutions: bool,
+
+	/// Print solving statistics
+	#[arg(short, long)]
+	statistics: bool,
+
+	/// Solving time limit
+	#[arg(short, long, value_parser = parse_time_limit)]
+	time_limit: Option<Duration>,
+
+	/// Level of verbosity
+	#[arg(short, long, action = clap::ArgAction::Count)]
+	verbose: u8,
+}
+
+fn parse_time_limit(s: &str) -> Result<Duration, humantime::DurationError> {
+	if let Ok(ms) = s.parse() {
+		Ok(Duration::from_millis(ms))
+	} else {
+		humantime::parse_duration(s)
+	}
 }
