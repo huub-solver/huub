6dcff84ba300855af56f6cde817d9cb554b5efc8
Allen Zhong <zhongzhw6@gmail.com>
2024-07-18 18:39:30 +1000

    Fix division with rounding functions for negative values

--- crates/huub/src/helpers.rs
+++ crates/huub/src/helpers.rs
@@ -6,9 +6,45 @@ use crate::{IntVal, NonZeroIntVal};
 
 #[inline]
 pub(crate) fn div_ceil(a: IntVal, b: NonZeroIntVal) -> IntVal {
-	a / b.get() + (0 != a % b.get()) as IntVal
+	let d = a / b.get();
+	let r = a % b.get();
+	if (r > 0 && b.get() > 0) || (r < 0 && b.get() < 0) {
+		d + 1
+	} else {
+		d
+	}
 }
 
 pub(crate) fn div_floor(a: IntVal, b: NonZeroIntVal) -> IntVal {
-	a / b.get()
+	let d = a / b.get();
+	let r = a % b.get();
+	if (r > 0 && b.get() < 0) || (r < 0 && b.get() > 0) {
+		d - 1
+	} else {
+		d
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::{
+		helpers::{div_ceil, div_floor},
+		NonZeroIntVal,
+	};
+
+	#[test]
+	fn test_div_ceil() {
+		assert_eq!(div_ceil(8, NonZeroIntVal::new(3).unwrap()), 3);
+		assert_eq!(div_ceil(-8, NonZeroIntVal::new(-3).unwrap()), 3);
+		assert_eq!(div_ceil(8, NonZeroIntVal::new(-3).unwrap()), -2);
+		assert_eq!(div_ceil(-8, NonZeroIntVal::new(3).unwrap()), -2);
+	}
+
+	#[test]
+	fn test_div_floor() {
+		assert_eq!(div_floor(8, NonZeroIntVal::new(3).unwrap()), 2);
+		assert_eq!(div_floor(-8, NonZeroIntVal::new(-3).unwrap()), 2);
+		assert_eq!(div_floor(8, NonZeroIntVal::new(-3).unwrap()), -3);
+		assert_eq!(div_floor(-8, NonZeroIntVal::new(3).unwrap()), -3);
+	}
 }
