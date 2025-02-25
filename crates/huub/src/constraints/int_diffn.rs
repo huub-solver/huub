f8d19d9dcdb667cde1e23447bd187307844c79d5
Danyal Mirza <danyalmirza@gmail.com>
2025-02-25 15:48:36 +0100

    added algorithm for adjusting the sweep point

--- crates/huub/src/constraints/int_diffn.rs
+++ crates/huub/src/constraints/int_diffn.rs
@@ -27,7 +27,7 @@ pub struct IntDiffnSweep {
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 // Active forbidden regions
-struct ForbiddenRegions {
+struct ForbiddenRegion {
 	lb: Vec<IntVal>, // lower bound of each dimension
 	ub: Vec<IntVal>  // upper bound of each dimension
 }
@@ -68,36 +68,132 @@ impl IntDiffnSweep {
         }
     }
 
+    fn prune_min<P: PropagationActions>(
+        actions: &mut P,
+        o_idx:usize
+    ) -> Result<(), Conflict> {
+        Ok(())
+    }
+    fn prune_max<P: PropagationActions>(
+        actions: &mut P,
+        o_idx:usize
+    ) -> Result<(), Conflict> {
+        Ok(())
+    }
+
+    fn adjust_sweep_min<P: PropagationActions>(
+        actions: &mut P,
+        sweep: &mut Vec<IntVal>,
+        jump: &mut Vec<IntVal>,
+        curr_obj_pos: &Vec<IntView>,
+        curr_dimension: usize,
+        dimensions: usize
+    ) -> bool {
+        for i in (0..dimensions).rev() {
+            let rotation = (i + curr_dimension) % dimensions;
+            sweep[rotation] = jump[rotation];
+            jump[rotation] = actions.get_int_upper_bound(curr_obj_pos[rotation]) + 1;
+            if sweep[rotation] <= actions.get_int_upper_bound(curr_obj_pos[rotation]) {
+                return true;
+            } else {
+                // Reset sweep-point
+                sweep[rotation] = actions.get_int_lower_bound(curr_obj_pos[rotation]);
+            }
+        }
+        false
+    }
+
+    fn adjust_sweep_max<P: PropagationActions>(
+        actions: &mut P,
+        sweep: &mut Vec<IntVal>,
+        jump: &mut Vec<IntVal>,
+        curr_obj_pos: &Vec<IntView>,
+        curr_dimension: usize,
+        dimensions: usize
+    ) -> bool {
+        for i in (0..dimensions).rev() {
+            let rotation = (i + curr_dimension) % dimensions;
+            sweep[rotation] = jump[rotation];
+            jump[rotation] = actions.get_int_lower_bound(curr_obj_pos[rotation]) - 1;
+            // Current sweep-point is withing the bounds of the current object
+            if sweep[rotation] <= actions.get_int_lower_bound(curr_obj_pos[rotation]) {
+                return true;
+            } else {
+                // Reset sweep-point
+                sweep[rotation] = actions.get_int_upper_bound(curr_obj_pos[rotation]);
+            }
+        }
+        false
+    }
+
+    
+
+    fn overlaps<P: PropagationActions>(
+        actions: &mut P,
+        curr_obj_pos: &Vec<IntView>,
+        fr: &ForbiddenRegion,
+        dimensions:usize
+    ) -> bool {
+        for d in 0..dimensions {
+            let no_overlap = actions.get_int_upper_bound(curr_obj_pos[d]) < fr.lb[d] ||
+                actions.get_int_lower_bound(curr_obj_pos[d]) > fr.ub[d];
+            if no_overlap {
+                return false;
+            }
+        }
+        true
+    } 
+
+        
+
+
     /// Generates forbidden regions given object o
-    fn gen_fr<P: PropagationActions>(&mut self, actions: &mut P, o_idx:usize) -> Option<ForbiddenRegions> {
-        let mut fr = ForbiddenRegions {
-            lb: Vec::new(),
-            ub: Vec::new()
-        };
-        let no_objects = self.box_posn.len();
-        let curr_obj_size = &self.box_size[o_idx];
+    fn generate_fr<P: PropagationActions>(
+        actions: &mut P,
+        o_idx:usize,
+        box_posn:Vec<Vec<IntView>>,
+        box_size:Vec<Vec<IntVal>>,
+        dimensions:usize
+    ) -> Option<Vec<ForbiddenRegion>> {
+        let mut all_fr:Vec<ForbiddenRegion> = vec![];
+        let no_objects = box_posn.len();
+        let curr_obj_size = &box_size[o_idx]; // get the address &?
         for i in 0..no_objects {
+            let mut fr = ForbiddenRegion {
+                lb: Vec::new(),
+                ub: Vec::new()
+            };
+
             if i == o_idx { continue };
-            let obj_pos = &self.box_posn[i];
-            let obj_size = &self.box_size[i];
+            let obj_pos = &box_posn[i];
+            let obj_size = &box_size[i];
+
             for (&pos, size, curr_size) in izip!(obj_pos, obj_size, curr_obj_size) { 
                 let pos_ub: IntVal = actions.get_int_upper_bound(pos); 
                 let pos_lb: IntVal = actions.get_int_lower_bound(pos); 
 
                 let fr_lb = pos_ub - curr_size + 1;
                 let fr_ub = pos_lb + size - 1; 
-                
-
+                if fr_lb <= fr_ub {
+                    fr.lb.push(fr_lb);
+                    fr.ub.push(fr_lb);
+                } else {
+                    break;
+                }
             }
-            // izip!(obj_pos, obj_size, curr_obj_size)
-            //     .filter(|(pos, size, curr_size)| {
-            //         actions.get_int_upper_bound(pos) - curr_obj_size + 1 <= actions.get_int_lower_bound(pos)
-            //     })
+
+            let is_overlapping = Self::overlaps::<P>(
+                actions,
+                &box_posn[o_idx],
+                &fr,
+                dimensions);
+
+            if fr.lb.len() == dimensions && is_overlapping{
+                all_fr.push(fr);
+            }
         }
-
-        Some(fr)
-
-
+        if all_fr.is_empty() { return None };
+        Some(all_fr)
     }
 
 }
